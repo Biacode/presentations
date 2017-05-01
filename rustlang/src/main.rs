580d387c8750b921def36c6eb818f7c6cd0070f2
Arthur Asatryan <biacoder@gmail.com>
2017-05-01 18:29:01 +0400

    #1 Added examples directory

--- rustlang/src/main.rs
+++ rustlang/src/main.rs
@@ -2,8 +2,6 @@ extern crate colored;
 
 use colored::*;
 
-// test the example with `cargo run --example most_simple`
 fn main() {
-    // TADAA !
     println!("{} {} !", "it".green(), "works".blue().bold());
 }
